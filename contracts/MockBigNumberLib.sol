b56029c3d8998e9a8032fec79cae1e023bd6a175
splut <riordant@tcd.ie>
2017-12-24 13:13:13 +0000

    mock contract additions

--- contracts/MockBigNumberLib.sol
+++ contracts/MockBigNumberLib.sol
@@ -81,4 +81,46 @@ contract MockBigNumberLib {
       
       return (res.val, res.neg, res.msb);
   }
+
+  //stack too deep error when passing in 9 distinct variables as arguments where 3 bignums are expected.
+  //instead we encode each msb/neg value in a bytes array and decode.
+  function mock_modmul(bytes a_val, bytes a_extra, bytes b_val, bytes b_extra, bytes mod_val, bytes mod_extra) public returns(bytes, bool, uint){    
+      BigNumberLib.BigNumber memory a;
+      BigNumberLib.BigNumber memory b;
+      BigNumberLib.BigNumber memory mod;
+    
+      uint neg;
+      uint msb;
+      
+      assembly {
+         neg := mload(add(a_extra,0x20))
+         msb := mload(add(a_extra,0x40))
+      }
+      
+      a.val = a_val;
+      a.msb = msb;
+      a.neg = (neg==1) ? true : false;
+      
+      assembly {
+         neg := mload(add(b_extra,0x20))
+         msb := mload(add(b_extra,0x40))
+      }
+      
+      b.val = b_val;
+      b.msb = msb;
+      b.neg = (neg==1) ? true : false;
+      
+      assembly {
+         neg := mload(add(mod_extra,0x20))
+         msb := mload(add(mod_extra,0x40))
+      }
+      
+      mod.val = mod_val;
+      mod.msb = msb;
+      mod.neg = (neg==1) ? true : false;
+    
+      BigNumberLib.BigNumber memory res = a.modmul(b,mod);
+      
+      return (res.val, res.neg, res.msb);
+  }
 }
